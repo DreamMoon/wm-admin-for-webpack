0b3b43d5c0dbb967b0818a24fe2be491b6190947
qq_15096707 <937921168@qq.com>
2017-02-09 21:21:49 +0800

    2017-02-09 删除无效文件

--- src/app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'pages', pathMatch: 'full' }
-];
-
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
